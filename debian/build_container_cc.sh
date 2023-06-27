048b364733f4e8379f5d8ddca145c14ab529df13
Derek Berger <dmberger.dev@gmail.com>
2023-06-27 08:34:08 -0400

    SHARE

--- debian/build_container_cc.sh
+++ debian/build_container_cc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 THIS_SCRIPT_PARENT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 cd "$THIS_SCRIPT_PARENT" || exit 1
-PILFER="$THIS_SCRIPT_PARENT/pilfer_container_files.sh"
 
 apptainer build --fakeroot --force debian_app.sif build_debian.def
